3e6f3fb1190e5df9c8e60940b94254ceea0d86a3
BenRubi0 <benjamin.rubio@malad.us>
2025-01-27 11:15:04 -0800

    Init

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    // create the readFile method to easily read the contents of a file
+    public static void readFile(String path) throws IOException {
+        File fileToRead = new File(path); // creates the file object for the file we're going to read
+        Scanner fileScanner = new Scanner(fileToRead); // creates the scanner object we're going to use to read the file
+
+        fileScanner.useDelimiter("\\Z"); // uses a delimiter to separate the file data
+        System.out.println(fileScanner.next()); // shows the user the contents of the next line
+    }
+
+    public static void main(String[] args) {
+        String fileToReadPath = "student.txt"; // store the path of the file we want to read
+
+        System.out.println("\nRead contents: \n"); // tells the user what content it read from the file
+
+        // try-catch statement since the readFile method throws an IOExeption
+        try {
+            readFile(fileToReadPath); // reads the file
+        } catch (IOException e) {
+            System.out.println(e.getMessage()); // tells the user what went wrong during the attempt to read the file
+        }
+    }
+}
