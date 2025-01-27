38f50c149191d7950ba159e39c1d69acf20069fd
BenRubi0 <benjamin.rubio@malad.us>
2025-01-27 11:16:38 -0800

    Update

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+// Made by Benjamin Rubio
+// For Mr. Gross Software and Programming Dev 2 Class
+// Contact at benjamin.rubio@malad.us
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
