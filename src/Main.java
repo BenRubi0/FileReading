0ed13d35455cfb866dcec61a039a44ef391770ba
BenRubi0 <benjamin.rubio@malad.us>
2025-02-04 10:02:31 -0800

    Update

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,12 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
+    public static Scanner fileScanner; // creates the public fileScanner variable
+
     // create the readFile method to easily read the contents of a file
     public static void readFile(String path) throws IOException {
         File fileToRead = new File(path); // creates the file object for the file we're going to read
-        Scanner fileScanner = new Scanner(fileToRead); // creates the scanner object we're going to use to read the file
+        fileScanner = new Scanner(fileToRead); // creates the scanner object we're going to use to read the file
 
         fileScanner.useDelimiter("\\Z"); // uses a delimiter to separate the file data
         System.out.println(fileScanner.next()); // shows the user the contents of the next line
@@ -27,5 +29,7 @@ public class Main {
         } catch (IOException e) {
             System.out.println(e.getMessage()); // tells the user what went wrong during the attempt to read the file
         }
+
+        fileScanner.close(); // close the reading stream
     }
 }
